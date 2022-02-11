d35203ed15ed8a3dd624503eadf37cb323966c3b
marcamvi <marta.cambero@gmail.com>
2022-02-11 15:51:42 +0100

    Ejercicios sprint1

---
+++ 21/index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $integer =2;
+        $double = 3.1415;
+        $string = "Wow, is this some kind of joke?";
+        $boolean = true;
+        
+         echo $integer."<br>"."$double"."<br>".$string."<br>".$boolean;
+        ?>
+    </body>
+</html>
