0884b30fefffd8a66362fd0678cbb0d9d6327c57
marcamvi <marta.cambero@gmail.com>
2022-03-14 09:34:14 +0100

    Corrección eliminación constructor

--- 4.2/Rectangle.php
+++ 4.2/Rectangle.php
@@ -2,10 +2,6 @@
 <?php
 
 class Rectangle extends Shape {
-    public function __construct($altura, $ancho) {
-        parent::__construct($altura, $ancho);
-    }
-
     public function area () {
         $area = ($this->width * $this->height);
         return $area;      
