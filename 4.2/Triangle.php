f00d8b41eafd5341e4fb2255dd50c401929290c0
marcamvi <marta.cambero@gmail.com>
2022-03-14 09:34:43 +0100

    Corrección eliminación constructor

--- 4.2/Triangle.php
+++ 4.2/Triangle.php
@@ -1,10 +1,7 @@
 <?php
 
 class Triangle extends Shape {
-    public function __construct($altura, $ancho) {
-        parent::__construct($altura, $ancho);
-    }
-    
+
     public function area () {
         $area = ($this->width * $this->height)/2;
         return $area;      
